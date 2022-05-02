1fcb41edc948d8c2347e8e6a9c53278dc75ee653
Oliver Boudet <oboudet1@me.com>
2022-05-02 13:37:00 +0200

    add image info

--- packages/backend/src/Controllers/Index.ts
+++ packages/backend/src/Controllers/Index.ts
@@ -2,10 +2,13 @@ import { app } from "../Index";
 import { CreateImageHandler } from "./api/image/create";
 import { DeleteImageHandler } from "./api/image/delete";
 import { ReadImagesHandler } from "./api/image/images";
+import { ReadImageHandler as ReadImageInfoHandler } from "./api/image/info";
 
 export function BindControllers() {
     // app.get("/api/items", CreateItemHandler);
     app.get("/images", ReadImagesHandler);
     app.post("/image/create", CreateImageHandler);
     app.delete("/image/delete/:id", DeleteImageHandler);
+    app.get("/image/:id/info", ReadImageInfoHandler);
+    // app.get("/image/:id", ReadImageHandler);
 }
