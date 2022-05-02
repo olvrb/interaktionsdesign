2793db970eb8126a74dd7c0e70df89ccccefcd23
David Jepsson <davjep-0@student.ltu.se>
2022-05-02 15:54:42 +0200

    created keywords entity

---
+++ packages/backend/src/Database/Entities/Keyword.ts
@@ -0,0 +1,12 @@
+import {
+    OneToMany, 
+    ManyToMany,
+    Entity,
+    
+} from "typeorm";
+import { BaseEntity } from "./BaseEntity";
+
+@Entity()
+export class Keyword extends BaseEntity {
+
+}
