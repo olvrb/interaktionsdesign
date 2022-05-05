3e9e9750d55aa39fcbbad7765c8aacdc3c06fdf2
David Jepsson <davjep-0@student.ltu.se>
2022-05-05 16:57:34 +0200

    Top bar start

---
+++ packages/frontend/src/components/TopBar.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+// Component which defines the top bar of the application
+<template>
+  <v-toolbar color="blue" elevation="4" rounded></v-toolbar>;
+</template>;
+</script>
