bffc8710ee5b246cbaed2260bbe90a72c519eb3a
agge <agnell-9@student.ltu.se>
2022-05-05 13:58:31 +0200

    testar git

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// Testar mvh Agnes
 import HelloWorld from "./components/HelloWorld.vue";
 import ImageBox from "./components/ImageBox.vue";
 import ImageViewer from "./components/ImageViewer.vue";
