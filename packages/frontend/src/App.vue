db7bb39cd2cb5a06556ce2d2d8ff9dfb327cb7ba
David Jepsson <davjep-0@student.ltu.se>
2022-05-06 11:53:34 +0200

    working on topbar

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-import ImageBox from "./components/ImageBox.vue";
-import ImageViewer from "./components/ImageViewer.vue";
+import { TopBar } from "./components/TopBar.vue";
+export default {
+  name: "App",
+  components: { TopBar },
+};
 </script>
 
 <template>
-  <HelloWorld msg="Nyhetssida" />
-  <ImageViewer />
+  <TopBar></TopBar>
 </template>
 
 <style>
--- packages/frontend/src/components/TopBar.vue
+++ packages/frontend/src/components/TopBar.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+export { default as TopBar } from "./components/TopBar.vue";
+
 // Component which defines the top bar of the application
-<template>
-  <v-toolbar color="blue" elevation="4" rounded></v-toolbar>;
-</template>;
 </script>
+
+<template>
+  <v-toolbar color="blue" elevation="4" rounded
+    ><v-app-bar-nav-icon></v-app-bar-nav-icon></v-toolbar
+  >; </template
+>;
