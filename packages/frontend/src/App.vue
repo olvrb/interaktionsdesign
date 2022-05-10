f2f166f2863e4601d4e15953a90aea75fa91feea
agge <agnell-9@student.ltu.se>
2022-05-10 11:44:39 +0200

    merging main

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-<<<<<<< HEAD
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-// Testar mvh Agnes
-import HelloWorld from "./components/HelloWorld.vue";
-import ImageBox from "./components/ImageBox.vue";
-import ImageViewer from "./components/ImageViewer.vue";
-=======
 import { TopBar } from "./components/TopBar.vue";
 export default {
   name: "App",
   components: { TopBar },
 };
->>>>>>> 3fce47acbaa7102238233a0c0f12da3907c157d9
 </script>
 
 <template>
