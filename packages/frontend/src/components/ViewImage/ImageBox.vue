44580137689222aa36bd33f25eee49143d3e373f
Oliver Boudet <oboudet1@me.com>
2022-05-16 10:52:13 +0200

    refactor

---
+++ packages/frontend/src/components/ViewImage/ImageBox.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { NImage } from "naive-ui";
+defineProps<{ title: string; description: string; imageSrc: string }>();
+</script>
+
+<template>
+  <div class="container">
+    <NImage class="image" width="200" v-bind:src="imageSrc" loading="lazy" />
+    <div class="middle">
+      <div class="text">{{ title }}</div>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  position: relative;
+  width: 50%;
+}
+
+.image {
+  opacity: 1;
+  display: block;
+  width: 100%;
+  height: auto;
+  transition: 0.5s ease;
+  backface-visibility: hidden;
+}
+
+.middle {
+  transition: 0.5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+}
+
+.container:hover .image {
+  opacity: 0.3;
+}
+
+.container:hover .middle {
+  opacity: 1;
+}
+</style>
